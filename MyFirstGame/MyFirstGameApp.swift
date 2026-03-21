ea61a4e4a6f790cb95de36f9a363ec596b4155e3
Noam <noamb99@gmail.com>
2026-03-21 22:01:45 +0200

    Initial Commit

---
+++ MyFirstGame/MyFirstGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstGameApp.swift
+//  MyFirstGame
+//
+//  Created by Noam on 21/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
